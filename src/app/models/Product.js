445bf830c38885f06220a9e97bef105b1b6c44ec
Felipe Soller <soller@outlook.com>
2021-01-08 18:45:57 -0300

    Criação da listagem e filtragem de produtos

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -82,5 +82,35 @@ module.exports = {
             SELECT * FROM files
             WHERE product_id = $1
         `, [id])
+    },
+    search(params) {
+        const { filter, category } = params
+
+        query = `
+            SELECT products.*,
+            categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            WHERE 1 = 1
+        `
+
+
+        if (category) {
+            query += ` AND products.category_id = ${category}`           
+        }
+
+        if (filter) {
+            query += ` 
+            AND
+            (products.name ILIKE '%${filter}%'
+            OR products.description ILIKE '%${filter}%')
+            `
+        }
+
+        query += `
+            AND status != 0
+        `
+
+        return db.query(query)
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ const routes = express.Router();
 const multer = require('./app/middlewares/multer');
 const ProductController = require('./app/controllers/ProductController');
 const HomeController = require('./app/controllers/HomeController');
+const SearchController = require('./app/controllers/SearchController');
 
+// HOME
 routes.get('/', HomeController.index);
 
+// SEARCH
+routes.get('/products/search', SearchController.index)
+
+// PRODUCTS
 routes.get('/products/create', ProductController.create);
 routes.get('/products/:id', ProductController.show)
 routes.post('/products', multer.array('photos', 6), ProductController.post);
@@ -13,8 +19,10 @@ routes.get('/products/:id/edit', ProductController.edit);
 routes.put('/products', multer.array('photos', 6), ProductController.put);
 routes.delete('/products', ProductController.delete);
 
+// ALIAS
 routes.get('/ads/create', function(require, response) {
     return response.redirect("/products/create");
 });
+
 
 module.exports = routes;
