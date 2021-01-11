2498a2ebd2f3c8deaa1ed93e558a0919ce47324a
Felipe Soller <soller@outlook.com>
2021-01-11 16:52:11 -0300

    Criação das rotas, controllers e formulário do registro de usuários

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const routes = express.Router();
+
+const HomeController = require('../app/controllers/HomeController');
+
+const products = require('./products');
+const users = require('./users');
+
+routes.get('/', HomeController.index);
+
+routes.use('/products', products)
+routes.use('/users', users)
+
+// ALIAS
+routes.get('/ads/create', function(require, response) {
+    return response.redirect("/products/create");
+});
+
+routes.get('/accounts', function(require, response) {
+    return response.redirect("/users/register");
+});
+
+module.exports = routes;
