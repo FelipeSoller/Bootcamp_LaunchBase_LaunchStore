be5057525b31ed1eb8c30fa871396924feb78260
Felipe Soller <soller@outlook.com>
2021-01-27 15:11:30 -0300

    Criação da funcionalidade de remoção de todos os items de uma vez do carrinho de compras

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -17,7 +17,7 @@ const Cart = {
         return this
     },
     addOne(product) {
-        let inCart = this.items.find(item => item.product.id == product.id)
+        let inCart = this.getCartItem(product.id)
 
         if (!inCart) {
             inCart = {
@@ -46,7 +46,7 @@ const Cart = {
         return this
     },
     removeOne(productId) {
-        const inCart = this.items.find(item => item.product.id == productId)
+        const inCart = this.getCartItem(productId)
 
         if (!inCart) return this
 
@@ -67,7 +67,22 @@ const Cart = {
         return this
     },
     delete(productId) {
+        const inCart = this.getCartItem(productId)
 
+        if (!inCart) return this
+
+        if (this.items.length > 0) {
+            this.total.quantity -= inCart.quantity
+            this.total.price -= (inCart.product.price * inCart.quantity)
+            this.total.formattedPrice = formatPrice(this.total.price)
+        }
+
+        this.items = this.items.filter(item => inCart.product.id != item.product.id)
+        
+        return this
+    },
+    getCartItem(productId) {
+        return this.items.find(item => item.product.id == productId)
     }
 }
 
