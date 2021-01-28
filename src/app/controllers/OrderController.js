d5b5342ae59068b930b62e1151eefb285be71133
Felipe Soller <soller@outlook.com>
2021-01-28 17:18:17 -0300

    Adição da funcionalidade da criação de pedidos

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,7 +1,9 @@
 const LoadProductService = require('../services/LoadProductService');
 const User = require('../models/User');
+const Order = require('../models/Order');
 
 const mailer = require('../../lib/mailer');
+const Cart = require('../../lib/cart');
 
 const email = (seller, product, buyer) => `
     <h2>Olá ${seller.name},</h2>
@@ -24,24 +26,50 @@ module.exports = {
     async post(req, res) {
 
         try {
-            const product = await LoadProductService.load('product', { where: {
-                id: req.body.id
-            }})
+            const cart = Cart.init(req.session.cart)
 
-            const seller = await User.findOne({where: {
-                id: product.user_id
-            }})
+            const buyer_id = req.session.userId
 
-            const buyer = await User.findOne({where: {
-                id: req.session.userId
-            }})
+            const filteredItems = cart.items.filter(item => item.product.user_id != buyer_id)
 
-            await mailer.sendMail({
-                to: seller.email,
-                from: 'no-reply@launchstore.com.br',
-                subject: 'Novo pedido de comprado',
-                html: email(seller, product, buyer)
+            const createOrdersPromise = filteredItems.map(async item => {
+                let { product, price:total, quantity } = item
+                const { price, id: product_id, user_id: seller_id } = product
+                const status = "open"
+
+                const order = await Order.create({
+                    seller_id,
+                    buyer_id,
+                    product_id,
+                    price,
+                    total,
+                    quantity,
+                    status
+                })
+
+                product = await LoadProductService.load('product', { where: {
+                    id: product_id
+                }})
+    
+                const seller = await User.findOne({where: {
+                    id: seller_id
+                }})
+    
+                const buyer = await User.findOne({where: {
+                    id: buyer_id
+                }})
+    
+                await mailer.sendMail({
+                    to: seller.email,
+                    from: 'no-reply@launchstore.com.br',
+                    subject: 'Novo pedido de comprado',
+                    html: email(seller, product, buyer)
+                })
+
+                return order
             })
+
+            await Promise.all(createOrdersPromise)           
 
             return res.render('orders/success')
 
